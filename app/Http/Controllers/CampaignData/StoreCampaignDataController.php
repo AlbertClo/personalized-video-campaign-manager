50c91925e6bf35d6ec2b7a22fe51a76cc753e01b
Albert Cloete <cloete.albert@gmail.com>
2024-06-11 06:01:05 +0200

    Return 404 when trying to add data to non-existent campaign.

--- app/Http/Controllers/CampaignData/StoreCampaignDataController.php
+++ app/Http/Controllers/CampaignData/StoreCampaignDataController.php
@@ -6,12 +6,18 @@ use App\Dtos\CampaignDataDto;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCampaignDataRequest;
 use App\Jobs\StoreCampaignDataJob;
+use App\Models\Campaign;
 use Illuminate\Http\JsonResponse;
 
 class StoreCampaignDataController extends Controller
 {
     public function __invoke(StoreCampaignDataRequest $request, $campaignId): JsonResponse
     {
+        $campaign = Campaign::whereId($campaignId)->first();
+        if ($campaign === null) {
+            return new JsonResponse(['data' => ['message' => "Campaign with ID {$campaignId} not found."]], 404);
+        }
+
         foreach ($request->data as $data) {
             StoreCampaignDataJob::dispatch(new CampaignDataDto(
                 $campaignId,
